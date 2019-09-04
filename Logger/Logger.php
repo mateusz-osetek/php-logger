dcef364ee07a0f331d6febdab77f949464bcd092
Mateusz Osetek <mateusz.osetek@eengine.pl>
2019-09-04 15:21:25 +0200

    Added new features, added PHPDoc

--- Logger/Logger.php
+++ Logger/Logger.php
@@ -24,63 +24,159 @@ class Logger implements LoggerInterface
      */
     protected $fullPath;
 
+    /**
+     * Logger constructor.
+     * @param string $path
+     * @param string $filename
+     * @param string $dateFormat
+     */
     public function __construct(string $path = 'logs/', string $filename = '.log', string $dateFormat = 'Y-m-d H:i:s')
     {
         $this->path = $path;
         $this->filename = $filename;
         $this->dateFormat = $dateFormat;
-        $this->fullPath = $this->formatPath();
+        $this->setFullPath();
     }
 
+    /**
+     * @param string $message
+     */
     public function put(string $message): void
     {
         $message = date($this->dateFormat) . " | {$message}\n";
-        file_put_contents($this->fullPath, $message, FILE_APPEND);
+        file_put_contents($this->getFullPath(), $message, FILE_APPEND);
     }
 
+    /**
+     * @param string $path
+     * @return string|null
+     */
     public function read(string $path = ''): ?string
     {
         if (empty($path)) {
-            $path = $this->fullPath;
+            $path = $this->getFullPath();
         }
         return file_get_contents($path);
     }
 
-    public function send(string $to): void
+    /**
+     * @param string $to
+     * @param string $subject
+     */
+    public function send(string $to, string $subject = ''): void
     {
+        if (empty($subject)) {
+            $subject = 'Log information from ' . date($this->dateFormat);
+        }
         mail(
             $to,
-            'Log information from ' . date($this->dateFormat),
+            $subject,
             $this->read()
         );
     }
 
+    /**
+     * @param string $to
+     * @param string $subject
+     * @param string $pathToFile
+     */
+    public function sendAsAttachment(string $to, string $subject, string $pathToFile): void
+    {
+        // TODO: Implement sendAsAttachment() method.
+    }
+
+    /**
+     * @param string $path
+     */
     public function wipe(string $path = ''): void
     {
         if (empty($path)) {
-            $path = $this->fullPath;
+            $path = $this->getFullPath();
         }
         file_put_contents($path, '');
     }
 
+    /**
+     * @param string $path
+     */
     public function drop(string $path = ''): void
     {
         if (empty($path)) {
-            $path = $this->fullPath;
+            $path = $this->getFullPath();
         }
         unlink($path);
     }
 
+    /**
+     * @return string|null
+     */
     public function getPath(): ?string
     {
+        return $this->path;
+    }
+
+    /**
+     * @param string|null $path
+     */
+    public function setPath(?string $path): void
+    {
+        $this->path = $path;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getFilename(): ?string
+    {
+        return $this->filename;
+    }
+
+    /**
+     * @param string|null $filename
+     */
+    public function setFilename(?string $filename): void
+    {
+        $this->filename = $filename;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDateFormat(): string
+    {
+        return $this->dateFormat;
+    }
+
+    /**
+     * @param string $dateFormat
+     */
+    public function setDateFormat(string $dateFormat): void
+    {
+        $this->dateFormat = $dateFormat;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFullPath(): string
+    {
+        $this->setFullPath();
         return $this->fullPath;
     }
 
+    private function setFullPath(): void
+    {
+        $this->fullPath = $this->formatPath();
+    }
+
+    /**
+     * @return string
+     */
     private function formatPath(): string
     {
         $this->path = str_replace('\\', '/', $this->path);
         $this->filename = str_replace(['\\', '/'], ['/', ''], $this->filename);
-        if ('/' !== substr($this->path, -1)) {
+        if (false === empty($this->path) && '/' !== substr($this->path, -1)) {
             $this->path .= '/';
         }
         return $this->path . $this->filename;
--- Logger/LoggerInterface.php
+++ Logger/LoggerInterface.php
@@ -17,8 +17,16 @@ interface LoggerInterface
 
     /**
      * @param string $to
+     * @param string $subject
      */
-    public function send(string $to): void;
+    public function send(string $to, string $subject): void;
+
+    /**
+     * @param string $to
+     * @param string $subject
+     * @param string $pathToFile
+     */
+    public function sendAsAttachment(string $to, string $subject, string $pathToFile): void;
 
     /**
      * @param string $path
@@ -34,4 +42,34 @@ interface LoggerInterface
      * @return string|null
      */
     public function getPath(): ?string;
+
+    /**
+     * @param string $path
+     */
+    public function setPath(?string $path): void;
+
+    /**
+     * @return string
+     */
+    public function getFullPath(): string;
+
+    /**
+     * @return string|null
+     */
+    public function getFilename(): ?string;
+
+    /**
+     * @param string|null $filename
+     */
+    public function setFilename(?string $filename): void;
+
+    /**
+     * @return string
+     */
+    public function getDateFormat(): string;
+
+    /**
+     * @param string $dateFormat
+     */
+    public function setDateFormat(string $dateFormat): void;
 }
